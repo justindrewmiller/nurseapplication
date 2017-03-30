4c21525489e9903a859f48cdcb2d40d43e90abcf
Justin <jmille23@my.westga.edu>
2017-03-30 02:33:17 -0400

    Implemented form design for Modify Patient form

--- UWG Healthcare/UWG Healthcare/View/ModifyPatient.Designer.cs
+++ UWG Healthcare/UWG Healthcare/View/ModifyPatient.Designer.cs
@@ -28,19 +28,234 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cboState = new System.Windows.Forms.ComboBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtZip = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtStreet = new System.Windows.Forms.TextBox();
+            this.txtSSN = new System.Windows.Forms.TextBox();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblphone = new System.Windows.Forms.Label();
+            this.lblzip = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.lblcity = new System.Windows.Forms.Label();
+            this.lblstreet = new System.Windows.Forms.Label();
+            this.lblssn = new System.Windows.Forms.Label();
+            this.lbllname = new System.Windows.Forms.Label();
+            this.lblfname = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // cboState
+            // 
+            this.cboState.FormattingEnabled = true;
+            this.cboState.Location = new System.Drawing.Point(127, 241);
+            this.cboState.Name = "cboState";
+            this.cboState.Size = new System.Drawing.Size(139, 21);
+            this.cboState.TabIndex = 37;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(127, 275);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(139, 20);
+            this.txtPhone.TabIndex = 36;
+            // 
+            // txtZip
+            // 
+            this.txtZip.Location = new System.Drawing.Point(127, 205);
+            this.txtZip.Name = "txtZip";
+            this.txtZip.Size = new System.Drawing.Size(139, 20);
+            this.txtZip.TabIndex = 35;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(127, 170);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(210, 20);
+            this.txtCity.TabIndex = 34;
+            // 
+            // txtStreet
+            // 
+            this.txtStreet.Location = new System.Drawing.Point(128, 136);
+            this.txtStreet.Name = "txtStreet";
+            this.txtStreet.Size = new System.Drawing.Size(209, 20);
+            this.txtStreet.TabIndex = 33;
+            // 
+            // txtSSN
+            // 
+            this.txtSSN.Location = new System.Drawing.Point(128, 104);
+            this.txtSSN.Name = "txtSSN";
+            this.txtSSN.Size = new System.Drawing.Size(139, 20);
+            this.txtSSN.TabIndex = 32;
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(128, 70);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(139, 20);
+            this.txtLName.TabIndex = 31;
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(128, 35);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(139, 20);
+            this.txtFName.TabIndex = 30;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(12, 9);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(115, 13);
+            this.lblUserName.TabIndex = 29;
+            this.lblUserName.Text = "UserNamePlaceHolder";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(214, 332);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 28;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(56, 332);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 27;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // lblphone
+            // 
+            this.lblphone.AutoSize = true;
+            this.lblphone.Location = new System.Drawing.Point(41, 278);
+            this.lblphone.Name = "lblphone";
+            this.lblphone.Size = new System.Drawing.Size(51, 13);
+            this.lblphone.TabIndex = 26;
+            this.lblphone.Text = "Phone #:";
+            // 
+            // lblzip
+            // 
+            this.lblzip.AutoSize = true;
+            this.lblzip.Location = new System.Drawing.Point(39, 205);
+            this.lblzip.Name = "lblzip";
+            this.lblzip.Size = new System.Drawing.Size(53, 13);
+            this.lblzip.TabIndex = 25;
+            this.lblzip.Text = "Zip Code:";
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(41, 241);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(35, 13);
+            this.lblState.TabIndex = 24;
+            this.lblState.Text = "State:";
+            // 
+            // lblcity
+            // 
+            this.lblcity.AutoSize = true;
+            this.lblcity.Location = new System.Drawing.Point(41, 167);
+            this.lblcity.Name = "lblcity";
+            this.lblcity.Size = new System.Drawing.Size(27, 13);
+            this.lblcity.TabIndex = 23;
+            this.lblcity.Text = "City:";
+            // 
+            // lblstreet
+            // 
+            this.lblstreet.AutoSize = true;
+            this.lblstreet.Location = new System.Drawing.Point(41, 136);
+            this.lblstreet.Name = "lblstreet";
+            this.lblstreet.Size = new System.Drawing.Size(38, 13);
+            this.lblstreet.TabIndex = 22;
+            this.lblstreet.Text = "Street:";
+            // 
+            // lblssn
+            // 
+            this.lblssn.AutoSize = true;
+            this.lblssn.Location = new System.Drawing.Point(41, 104);
+            this.lblssn.Name = "lblssn";
+            this.lblssn.Size = new System.Drawing.Size(32, 13);
+            this.lblssn.TabIndex = 21;
+            this.lblssn.Text = "SSN:";
+            // 
+            // lbllname
+            // 
+            this.lbllname.AutoSize = true;
+            this.lbllname.Location = new System.Drawing.Point(41, 73);
+            this.lbllname.Name = "lbllname";
+            this.lbllname.Size = new System.Drawing.Size(61, 13);
+            this.lbllname.TabIndex = 20;
+            this.lbllname.Text = "Last Name:";
+            // 
+            // lblfname
+            // 
+            this.lblfname.AutoSize = true;
+            this.lblfname.Location = new System.Drawing.Point(41, 38);
+            this.lblfname.Name = "lblfname";
+            this.lblfname.Size = new System.Drawing.Size(60, 13);
+            this.lblfname.TabIndex = 19;
+            this.lblfname.Text = "First Name:";
             // 
             // ModifyPatient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(353, 344);
+            this.ClientSize = new System.Drawing.Size(365, 378);
+            this.Controls.Add(this.cboState);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtZip);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtStreet);
+            this.Controls.Add(this.txtSSN);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblphone);
+            this.Controls.Add(this.lblzip);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.lblcity);
+            this.Controls.Add(this.lblstreet);
+            this.Controls.Add(this.lblssn);
+            this.Controls.Add(this.lbllname);
+            this.Controls.Add(this.lblfname);
             this.Name = "ModifyPatient";
             this.Text = "Modify Patient";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cboState;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtZip;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtStreet;
+        private System.Windows.Forms.TextBox txtSSN;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblphone;
+        private System.Windows.Forms.Label lblzip;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblcity;
+        private System.Windows.Forms.Label lblstreet;
+        private System.Windows.Forms.Label lblssn;
+        private System.Windows.Forms.Label lbllname;
+        private System.Windows.Forms.Label lblfname;
     }
 }
