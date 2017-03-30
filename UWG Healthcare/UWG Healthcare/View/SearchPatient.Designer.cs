4d0e6794c4991d028d5c87297640568464ae0646
Justin <jmille23@my.westga.edu>
2017-03-30 02:47:34 -0400

    Search Patient form design completed

--- UWG Healthcare/UWG Healthcare/View/SearchPatient.Designer.cs
+++ UWG Healthcare/UWG Healthcare/View/SearchPatient.Designer.cs
@@ -28,19 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblSearchName = new System.Windows.Forms.Label();
+            this.lblDOB = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDOB = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnGoTo = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(12, 9);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(115, 13);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "UserNamePlaceHolder";
+            // 
+            // lblSearchName
+            // 
+            this.lblSearchName.AutoSize = true;
+            this.lblSearchName.Location = new System.Drawing.Point(12, 63);
+            this.lblSearchName.Name = "lblSearchName";
+            this.lblSearchName.Size = new System.Drawing.Size(35, 13);
+            this.lblSearchName.TabIndex = 1;
+            this.lblSearchName.Text = "Name";
+            // 
+            // lblDOB
+            // 
+            this.lblDOB.AutoSize = true;
+            this.lblDOB.Location = new System.Drawing.Point(12, 119);
+            this.lblDOB.Name = "lblDOB";
+            this.lblDOB.Size = new System.Drawing.Size(33, 13);
+            this.lblDOB.TabIndex = 2;
+            this.lblDOB.Text = "DOB:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(89, 63);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(140, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtDOB
+            // 
+            this.txtDOB.Location = new System.Drawing.Point(89, 116);
+            this.txtDOB.Name = "txtDOB";
+            this.txtDOB.Size = new System.Drawing.Size(140, 20);
+            this.txtDOB.TabIndex = 4;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(107, 156);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(224, 345);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnGoTo
+            // 
+            this.btnGoTo.Location = new System.Drawing.Point(12, 345);
+            this.btnGoTo.Name = "btnGoTo";
+            this.btnGoTo.Size = new System.Drawing.Size(96, 23);
+            this.btnGoTo.TabIndex = 7;
+            this.btnGoTo.Text = "Select Patient";
+            this.btnGoTo.UseVisualStyleBackColor = true;
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(38, 201);
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ReadOnly = true;
+            this.txtResults.Size = new System.Drawing.Size(233, 96);
+            this.txtResults.TabIndex = 8;
+            this.txtResults.Text = "";
             // 
             // SearchPatient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(364, 342);
+            this.ClientSize = new System.Drawing.Size(354, 380);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.btnGoTo);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtDOB);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblDOB);
+            this.Controls.Add(this.lblSearchName);
+            this.Controls.Add(this.lblUserName);
             this.Name = "SearchPatient";
             this.Text = "Search Patient";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblSearchName;
+        private System.Windows.Forms.Label lblDOB;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDOB;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnGoTo;
+        private System.Windows.Forms.RichTextBox txtResults;
     }
 }
