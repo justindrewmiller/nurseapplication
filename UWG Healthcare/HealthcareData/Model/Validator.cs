6231d0d11bbba346aefa2c818e4c9f191d104e7b
Phillip Thomas <pthomas5@my.westga.edu>
2017-04-01 21:41:42 -0400

    Created Validator class to validate information before insertion

---
+++ UWG Healthcare/HealthcareData/Model/Validator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HealthcareData.Model
+{   // Class that is used to validate information.
+    class Validator
+    {
+        private static string title = "Entry Error";
+
+        // Returns title of the message box
+        public static string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        // Checks that the information is present
+        public static bool IsPresent(Control control)
+        {
+            if (control.GetType().ToString() == "System.Windows.Forms.TextBox")
+            {
+                TextBox textBox = (TextBox)control;
+                if (string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    MessageBox.Show(textBox.Tag.ToString() + " is a required field.", Title);
+                    textBox.Focus();
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else if (control.GetType().ToString() == "System.Windows.Forms.ComboBox")
+            {
+                ComboBox comboBox = (ComboBox)control;
+                if (comboBox.SelectedIndex == -1)
+                {
+                    MessageBox.Show(comboBox.Tag.ToString() + " is a required field.", Title);
+                    comboBox.Focus();
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return true;
+        }
+
+
+        public static bool IsInt32(TextBox textBox)
+        {
+            try
+            {
+                if (textBox.Text.All(char.IsDigit))
+                {
+                    Convert.ToInt32(textBox.Text);
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show(textBox.Tag.ToString() + " must be an integer value.", Title);
+                    textBox.Focus();
+                    return false;
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show(textBox.Tag.ToString() + " must be an integer value.", Title);
+                textBox.Focus();
+                return false;
+            }
+        }
+    }
+}
