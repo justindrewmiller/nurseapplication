542eb9e64db01190727cf0fb588fbeb67db1760a
Phillip Thomas <pthomas5@my.westga.edu>
2017-04-04 20:15:47 -0400

    Created Doctor Controller

---
+++ UWG Healthcare/UWG Healthcare/Controller/DoctorController.cs
@@ -0,0 +1,21 @@
+﻿using HealthcareData.DAL;
+using HealthcareData.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWG_Healthcare.Controller
+{
+    public class DoctorController
+    {
+        // Gets the Doctors list
+        public List<Doctor> GetDoctors()
+        {
+            return DoctorDAL.GetDoctors();
+        }
+    }
+
+ 
+}
