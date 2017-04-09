01767890721345108e461e7dbaf109539765ddc5
DESKTOP-PJJLTO9\Justin <jmille23@my.westga.edu>
2017-04-09 00:21:08 -0400

    Controller for Search form completed

---
+++ UWG Healthcare/UWG Healthcare/Controller/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using HealthcareData.DAL;
+using HealthcareData.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWG_Healthcare.Controller
+{
+    public class SearchController
+    {
+        public static Patient SearchPatientsByDOB(string DOB)
+        {
+            return PatientDAL.SearchPatientByDOB(DOB);
+        }
+
+        public static Patient SearchPatientsByFullName(string FName, string LName)
+        {
+            return PatientDAL.SearchPatientByFullName(FName, LName);
+        }
+
+        public static Patient SearchPatientsByLastNameAndDOB(string DOB, string LName)
+        {
+            return PatientDAL.SearchPatientByLastNameAndDOB(DOB, LName);
+        }
+    }
+}
