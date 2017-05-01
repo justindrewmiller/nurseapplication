5c8d54ed6262a1ff50c548c0fb471a66c5bc19f1
DESKTOP-PJJLTO9\Justin <jmille23@my.westga.edu>
2017-05-01 19:41:07 -0400

    user info now appears in the menu strip

--- UWG Healthcare/UWG Healthcare/View/MainContainer.cs
+++ UWG Healthcare/UWG Healthcare/View/MainContainer.cs
@@ -18,6 +18,7 @@ namespace UWG_Healthcare
 
         UserInfo userLoggedIn;
         String role = "";
+        ToolStripLabel stripLabel;
 
         private HealthcareController inController;
         public MainContainer(UserInfo user)
@@ -89,13 +90,10 @@ namespace UWG_Healthcare
 
         private void MainContainer_Load(object sender, EventArgs e)
         {
-            //if (role == "NURSE")
-            //{
-            //    MenuScreen ms = new MenuScreen(userLoggedIn);
-            //    ms.MdiParent = this;
-            //    ms.Show();
-            //}
-            
+            this.stripLabel = new ToolStripLabel(lblUserID.Text);
+            menuStrip1.Items.Add(this.stripLabel);
+            this.stripLabel.Alignment = 
+
         }
 
         private void logIn_FormClosed(object sender, FormClosedEventArgs e)
@@ -109,12 +107,6 @@ namespace UWG_Healthcare
             li.Show();
         }
 
-        private void menuScreenToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            MenuScreen ms = new MenuScreen(userLoggedIn);
-            ms.MdiParent = this;
-            ms.Show();
-        }
 
         // Generates form for running the admin report
         private void generateReportToolStripMenuItem_Click(object sender, EventArgs e)
@@ -124,6 +116,5 @@ namespace UWG_Healthcare
             gr.Show();
 
         }
-    
     }
 }
